a8df1e183aba334ee180d41f7247ca80556c1729
MikeVV <w.nanthakon@gmail.com>
2016-05-27 21:22:44 +0700

    building a registration page

--- index.php
+++ index.php
@@ -20,7 +20,14 @@ else $signIn = false; // 1 = true , other false
 <div id="partial" class="container-fluid" style="margin-top: 10vh">
     <?php
     if ($signIn) {
-        echo file_get_contents("partials/todo.html");
+        if($_SESSION['username'] == "WittyWitty" OR $_SESSION['username'] == "wittywitty"){?>
+            <div class="row">
+                <div class="col-sm-4 col-sm-offset-4 alert alert-info">
+                    <p class="text-center"><a  href="http://angsila.cs.buu.ac.th/~57160438/887371/Sticky/source/source.zip" >source.zip</a></p>
+                </div>
+            </div>
+       <?php }
+    echo file_get_contents("partials/todo.html");
     } else {
         echo file_get_contents("partials/signIn.html");
     }
